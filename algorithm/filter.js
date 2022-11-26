ac6811c20a492896a7fa18b1bcd7c0b9a86eabca
Sanim Khatri <076bei037.sanim@pcampus.edu.np>
2022-11-26 18:28:48 +0545

    filtering algorithm added

---
+++ algorithm/filter.js
@@ -0,0 +1,17 @@
+
+export default function Filtering(x){
+    console.log(x);
+    var len=x.length;
+    var s1=0;
+    var temp=[];
+    for(var i=0;i<len;i++){
+        s1+=x[i][1];
+    }
+    for(var i=0;i<len;i++){
+        if(x[i][1]/s1 >=0.03){
+            temp.push(x[i]);
+        }
+    }
+    console.log(temp);
+    return temp;
+}
