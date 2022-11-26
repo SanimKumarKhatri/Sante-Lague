478f52eabb5094aef8e84527b8acdc701b47d51b
Sanim Khatri <076bei037.sanim@pcampus.edu.np>
2022-11-26 14:59:12 +0545

    Algorithm Completed

--- algorithm/sainte-lague.js
+++ algorithm/sainte-lague.js
@@ -1,18 +1,56 @@
 
+function sum(s){
+    var sum=0;
+    let len=s.length;
+    for(var i=0;i<len;i++){
+        sum+=s[i];
+    }
+    return sum;
+}
+function max(f){
+    var max=-Infinity;
+    let len=f.length;
+    for(var i=0;i<len;i++){
+        if(f[i]>max){
+            max=f[i];
+        }
+    }
+    return max;
+}
 function sainte_lague(x, seats){
     //main formula
-    let voteresult = x;
-    var s=0;
-    for(var i=0; i<3;i++){
-    while(voteresult[i][1]>seats){
-        console.log(voteresult[i][0]);
-        console.log(voteresult[i][1]);
-        voteresult[i][1] = voteresult[i][1]/(2*s+1.4);
-        s++;
+    var v= new Array();
+    var t=new Array();
+    var result=[];
+    var s= new Array();
+    let len = x.length;
+    for(var i =0; i<len;i++){
+        t[i]=v[i]=x[i][1];
+        s[i]=0;
     }
+    console.log(v);
+    console.log(s);
+    while(sum(s)<seats){
+        if(sum(s)==0){
+            for(var i=0; i<len;i++){
+                v[i]=t[i]/((2*s[i])+1);
+            }
+        }
+        var m=max(v);
+        for(var i=0;i<len;i++){
+            if(v[i]==m){
+                s[i]++;
+                v[i]=t[i]/((2*s[i])+1);
+            }
+        }
+        console.log(v);
+        console.log(s);
     }
-    //algorithm to assign number of seats to be added
-    return s;
+
+    for(var i=0;i<len;i++){
+        result[i]=[x[i][0],x[i][1],s[i]];
+    }
+    return result;  
 }
 
 export default sainte_lague;
